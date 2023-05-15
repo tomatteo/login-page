b44fae2bc46f4fa4075cabf12223a157cf7dbf7c
tomatteo <133718412+tomatteo@users.noreply.github.com>
2023-05-15 20:26:36 -0300

    Add files via upload

---
+++ loginpage.js
@@ -0,0 +1,9 @@
+function logar() {
+    var nome = document.getElementById('nome1').value; 
+    var senha = document.getElementById('senha1').value;
+    var matricula = document.getElementById('registro1').value;
+    var area = document.getElementById('area1').value;
+    var email = document.getElementById('email1').value; 
+    var cargahoraria = document.getElementById('carga1').value;
+    console.log(nome);
+}
